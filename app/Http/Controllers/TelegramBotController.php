32ff9268559ba0417de0387436a7f7d70d718686
Nikolay <reddeveloper@gmail.com>
2022-03-23 20:10:31 +0300

    init telegramm bot

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use TelegramBot\Api\BotApi;
 use TelegramBot\Api\Exception;
+use TelegramBot\Api\InvalidArgumentException;
 
 class TelegramBotController extends Controller
 {
@@ -23,13 +24,17 @@ class TelegramBotController extends Controller
     /**
      * @param Request $request
      * @return void
+     * @throws Exception
+     * @throws InvalidArgumentException
      */
     public function getMessage(Request $request)
     {
-        Log::debug('tg-message', $request->all());
-        $updates = json_decode(file_get_contents('php://input'), 1);
+        $bot = new BotApi('5174671948:AAGbDuirO7x92Ai3W--aiYkd-WmCol1IW70');
+        $updates = $request->all();
         $textUpdates = $updates['message']['text'];
-//        $chatId = $updates['message'] ? $updates['message']['chat']['id'] : $updates['callback_query']['from']['id']; // чат от которого пришло сообщение
+        $chatId = $updates['message'] ? $updates['message']['chat']['id'] : $updates['callback_query']['from']['id']; // чат от которого пришло сообщение
         Log::debug('tg-message', [$textUpdates]);
+        Log::debug('chatId', [$chatId]);
+        $bot->sendMessage($chatId, 'Сообщение получено');
     }
 }
